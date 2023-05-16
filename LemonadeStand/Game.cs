c2ebe98dd32f0c5f6b7f0740d767454b47d24df4
Benjameslane <ben.james.lane9>
2023-05-16 09:51:47 -0600

    Walked through an implemented UML framework into the sln file

---
+++ LemonadeStand/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    internal class Game
+    {
+        //Member Variables (Has A)
+        private Player player;
+        //private List<Day> Days;
+        private int currentDay;
+
+        //Constructor
+        public Game()
+        {
+            
+        }
+
+        //Member Methods (Can Do)
+    }
+}
